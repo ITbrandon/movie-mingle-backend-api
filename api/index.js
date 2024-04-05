606e65dd1894f0e37819328ca1a6b8485bc74e16
Itbrandon <brandoniticka@gmail.com>
2024-04-05 08:36:15 -0700

    removed-mongoose-package

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import mongoose from "mongoose";
-// import cors from "cors";
+// import mongoose from "mongoose";
+import cors from "cors";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
@@ -11,26 +11,26 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(
-//   cors({
-//     origin: "",
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use("/api/posts", postRoute);
 
-mongoose
-  .connect(
-    "mongodb+srv://brandoniticka:eCMyRjhRmZ4fGn6X@backenddb.qx5crjn.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
-  )
-  .then(() => {
-    console.log("Connected!");
-    app.listen(3000, () => {
-      console.log("Server Listening on Port 3000");
-    });
-  })
-  .catch(() => {
-    console.log("Failed to Connect");
-  });
+// mongoose
+//   .connect(
+//     "mongodb+srv://brandoniticka:eCMyRjhRmZ4fGn6X@backenddb.qx5crjn.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+//   )
+//   .then(() => {
+//     console.log("Connected!");
+//     app.listen(3000, () => {
+//       console.log("Server Listening on Port 3000");
+//     });
+//   })
+//   .catch(() => {
+//     console.log("Failed to Connect");
+//   });
--- api/models/post.model.js
+++ api/models/post.model.js
@@ -1,22 +1,22 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
+// const postSchema = mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
 
-  content: {
-    type: String,
-    required: true,
-  },
+//   content: {
+//     type: String,
+//     required: true,
+//   },
 
-  image: {
-    type: String,
-    required: false,
-  },
-});
+//   image: {
+//     type: String,
+//     required: false,
+//   },
+// });
 
-const Post = mongoose.model("Post", postSchema);
+// const Post = mongoose.model("Post", postSchema);
 
-export default Post;
+// export default Post;
